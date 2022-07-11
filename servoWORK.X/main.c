6bdae6cb398f73be81152014934723677a1b3c55
Christopher Yonek <58499404+ChristopherMathematica@users.noreply.github.com>
2022-07-10 20:53:54 -0400

    Update main.c

--- servoWORK.X/main.c
+++ servoWORK.X/main.c
@@ -1,3 +1,9 @@
+//ELC275 Microprocessor Systems (Delaware Technical Community College)
+//Student: Christopher Yonek
+//Assignment: This is the driver code for the Robotic coin sorter.
+//Devices Used: Basys MX3, Servo Motors, and Strain Gauge Load Cell (HX711 ACD)
+//Date: April 2021
+
 # include <p32xxxx.h>
 # include <xc.h>
 # include "Run.h"
